1200fa0f1c0854e3816bf4065aaa65fcb1303bad
Soundleo <leorperes@gmail.com>
2024-06-05 19:48:45 -0300

    Fatorial 1.0

---
+++ main.py
@@ -0,0 +1,13 @@
+# funcao fatorial
+def calcular_fatorial(n):
+    if n == 0: 
+        return 1
+    else:
+      return n * calcular_fatorial(n-1)
+
+# programa principal 
+
+n = int(input('Informe um número inteiro: '))
+
+print(f'Fatorial de {n}: {calcular_fatorial(n)}')
+    
